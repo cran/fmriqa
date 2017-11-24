969b39a2073305e4273d63b93e00c46a6f5bca1d
Martin Wilson <martin@pipegrep.co.uk>
2017-11-24 12:51:41 +0000

    version 0.1.0

---
+++ R/fmriqa.R
@@ -0,0 +1,10 @@
+#' fmriqa: fMRI quality assurance routines
+#'
+#' The fmriqa package provides an implementation of the fMRI quality assurance
+#' analysis protocol detailed by Friedman and Glover (2006)
+#' <doi:10.1002/jmri.20583>.
+#'
+#' For a full list of functions:
+#' `help(package=fmriqa, help_type="html")`
+#'
+"_PACKAGE"
